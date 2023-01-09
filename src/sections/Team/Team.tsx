94459f061abba8d2b6439453305ac9c542907821
evgenij <onethps@gmail.com>
2023-01-09 22:03:37 +0200

    dev init

--- src/sections/Team/Team.tsx
+++ src/sections/Team/Team.tsx
@@ -6,18 +6,16 @@ export const Team = () => {
   return (
     <section className={styles.root}>
       <div className={styles.wrapper}>
-        <div className={styles.header}>
+        <div className={styles.headerBox}>
           <h1 className={styles.title}>Our Top Team</h1>
           <p className={styles.description}>
             Learn more about how you can save our planets nature.
           </p>
         </div>
-        <div className={styles.photosBlock}>
-          <div className={styles.userImages}>
-            <img className={styles.user} alt="user_img" src="./user1.jpg" />
-            <img className={styles.user} alt="user_img" src="./user2.jpg" />
-            <img className={styles.user} alt="user_img" src="./user3.jpg" />
-          </div>
+        <div className={styles.usersBox}>
+          <img className={styles.user} alt="user_img" src="./user1.jpg" />
+          <img className={styles.user} alt="user_img" src="./user2.jpg" />
+          <img className={styles.user} alt="user_img" src="./user3.jpg" />
         </div>
       </div>
     </section>
