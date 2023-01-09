f6daede5ea9c87f983e53d869258d6f812be55bf
evgenij <onethps@gmail.com>
2023-01-09 19:26:25 +0200

    🐞 fix(header menu):
    fix scroll lock

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import IconLocation from '../../assets/icon-location.svg';
+import { ReactComponent as IconLocation } from '../../assets/icon-location.svg';
 import { Button } from '../Button/Button';
 
 import styles from './SearchInput.module.scss';
@@ -9,7 +9,7 @@ import { SearchInputProps } from './SearchInput.Props';
 export const SearchInput: React.FC<SearchInputProps> = ({ className, ...props }) => {
   return (
     <div {...props} className={`${className} ${styles.root}`}>
-      <img alt={IconLocation} src={IconLocation} />
+      <IconLocation width={14} height={20} />
       <input placeholder="Find the place to help" />
       <Button variant="primary">SEARCH</Button>
     </div>
--- src/sections/Header/Header.tsx
+++ src/sections/Header/Header.tsx
@@ -1,20 +1,70 @@
 import React from 'react';
 
-import BurgerIcon from '../../assets/burger.svg';
+import { ReactComponent as BurgerIcon } from '../../assets/burger.svg';
 import { Button } from '../../components/Button/Button';
 import { Logo } from '../../components/Logo/Logo';
-import { Navigation } from '../../components/Navigation/Navigation';
+// import { Navigation } from '../../components/Navigation/Navigation';
 
 import styles from './Header.module.scss';
 
 export const Header = () => {
+  const [open, setOpen] = React.useState<boolean>(false);
+
+  const links = [
+    {
+      id: 1,
+      title: 'Home',
+      href: '/',
+      active: true,
+    },
+    {
+      id: 2,
+      title: 'Our mission',
+      href: '/',
+      active: false,
+    },
+    {
+      id: 3,
+      title: 'Places',
+      href: '/',
+      active: false,
+    },
+    {
+      id: 4,
+      title: 'Team',
+      href: '/',
+      active: false,
+    },
+  ];
+
+  React.useEffect(() => {
+    if (open) document.body.style.overflow = 'hidden';
+    else document.body.removeAttribute('style');
+
+    return () => {
+      document.body.removeAttribute('style');
+    };
+  }, [open]);
+
   return (
     <header className={styles.header}>
       <div className={styles.wrapper}>
         <Logo className={styles.logo} />
-        <Navigation className={styles.navigation} />
-        <Button variant="secondary">Apply</Button>
-        <img alt="burger" className={styles.burgerIcon} src={BurgerIcon} />
+        {/* <Navigation className={styles.navigation} /> */}
+
+        <nav className={`${styles.linkBox} ${open ? styles.active : ''}`}>
+          {links.map(link => (
+            <a href={link.href} key={link.id}>
+              {link.title}
+            </a>
+          ))}
+          <Button variant="secondary">Apply</Button>
+        </nav>
+        <BurgerIcon className={styles.burgerIcon} onClick={() => setOpen(!open)} />
+
+        <Button className={styles.navButton} variant="secondary">
+          Apply
+        </Button>
       </div>
     </header>
   );
