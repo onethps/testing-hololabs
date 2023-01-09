5dbbacf246360edae11851769158df556da162a9
evgenij <onethps@gmail.com>
2023-01-09 19:26:25 +0200

    🐞 fix(header menu):
    fix scroll lock

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -5,7 +5,7 @@ import { LogoProps } from './Logo.Props';
 
 export const Logo: React.FC<LogoProps> = ({ className, ...props }) => {
   return (
-    <div {...props} className={`${styles.root} ${className}`}>
+    <div {...props} className={`${className} ${styles.root} `}>
       <img width={38} height={38} alt="logo" src="./logo.svg" />
       <h1>Rootz</h1>
     </div>
